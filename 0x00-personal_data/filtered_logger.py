abb72480c987a5e44044302ef394ac96bdee27c1
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-29 22:49:39 +0000

    To format log records

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Filtering and Logging data. """
 import re
+import logging
 from typing import List
 
 
@@ -9,3 +10,22 @@ def filter_datum(fields: List[str], redaction: str,
     """ Returns a log message obfuscated. """
     pattern = r'(' + r'|'.join(fields) + r')=(.*?)' + separator
     return re.sub(pattern, r'\1=' + redaction + separator, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self._fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Returns a string of redacted log records. """
+        rec = filter_datum(self._fields, self.REDACTION,
+                           super().format(record), self.SEPARATOR)
+        return re.sub(r'' + self.SEPARATOR, self.SEPARATOR + " ", rec)
