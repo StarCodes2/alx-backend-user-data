52f606c6680b9f245b27fe5102e147da7abf2d68
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-30 01:11:17 +0000

    To check if a string matches the hashed password.

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     """ Returns an encrypted string(password). """
     text = password.encode('utf-8')
     return bcrypt.hashpw(text, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates if password matches the hashed password. """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
