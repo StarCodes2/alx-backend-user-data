b0618a75ad93e9acbcb8db134dad6310b368ee6d
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-03 22:36:14 +0000

    To search for a user using email and password

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ Basic Authentication """
 import base64
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -43,3 +45,19 @@ class BasicAuth(Auth):
 
         new_split = decoded_base64_authorization_header.split(":")
         return (new_split[0], new_split[1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """ Checks if user with specified credentials exist. """
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+
+        users = User.search({"email": user_email})
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+
+        return None
