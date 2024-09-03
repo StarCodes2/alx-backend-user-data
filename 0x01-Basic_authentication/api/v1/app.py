b9950c0d32b15321d79496cfe940fb3d067a3dd5
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-03 16:19:25 +0000

    Validating requests

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,25 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = os.getenv("AUTH_TYPE", None)
+if auth:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
+
+@app.before_request
+def before_req() -> None:
+    """ Filters all requests. """
+    paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+    if auth and auth.require_auth(request.path, paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(404)
