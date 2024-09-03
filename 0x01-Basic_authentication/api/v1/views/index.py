8b7fa6357cc9d45b0132effe5d89491ff39e075e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-03 12:08:00 +0000

    To handle unauthorized to a resource

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -30,3 +30,9 @@ def stats() -> str:
 def unauthorized() -> None:
     """ Raises a 401 error by using abort. """
     abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> None:
+    """ Raises a 403 error by using about. """
+    abort(403)
