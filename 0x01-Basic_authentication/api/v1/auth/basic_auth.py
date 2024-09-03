7a1ebe6f57605628060f179c6a7e5c9761f2ffc2
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-03 17:49:12 +0000

    To extract user credentials from a string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -29,3 +29,17 @@ class BasicAuth(Auth):
             return None
 
         return decoded_str.decode("utf-8")
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> (str, str):
+        """ To extract the user credentials from a string. """
+        if decoded_base64_authorization_header is None\
+                or type(decoded_base64_authorization_header) != str:
+            return (None, None)
+
+        if ":" not in decoded_base64_authorization_header:
+            return (None, None)
+
+        new_split = decoded_base64_authorization_header.split(":")
+        return (new_split[0], new_split[1])
