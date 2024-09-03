4f85da0790773c2517a697e3fc35d820c59ba1b4
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-03 17:03:51 +0000

    To extract the basic authentication string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,11 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """ Define methods to authrnticate users using basic authentication. """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ To extract Base64 authentication string. """
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header[:6] != "Basic ":
+            return None
+        return authorization_header[6:]
