d64ac8522a73e23164c4457f7a7c6bbf065788e3
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-05 16:50:38 +0000

    Edge case: No user with specified email.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -58,7 +58,7 @@ class BasicAuth(Auth):
             return None
 
         users = User.search({"email": user_email})
-        if not users:
+        if not users or len(users) == 0:
             return None
         if users[0] and users[0].is_valid_password(user_pwd):
             return users[0]
