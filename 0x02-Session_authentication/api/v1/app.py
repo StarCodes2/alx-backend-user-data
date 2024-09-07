73b50b6faac84dd425969fd6c45a8dc44cf1e97e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-07 15:39:58 +0000

    To handle invalid session id

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -30,10 +30,12 @@ def before_req() -> None:
     paths = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
     ]
     if auth is not None and auth.require_auth(request.path, paths):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and\
+             auth.session_cookie(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
