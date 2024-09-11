2bfb3879396b60cbdc838cb17b49315583fd25a1
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-11 15:50:48 +0000

    To add a new user to the database.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -31,9 +31,6 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """ Add a new user. """
-        if not email or not hashed_password:
-            return None
-
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
