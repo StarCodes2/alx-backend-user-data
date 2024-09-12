2b4071b98c9235654daa8882151b577cff5f96b1
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-12 22:06:53 +0000

    FEAT: Session login

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Flask App """
 from auth import Auth
-from flask import Flask, request, jsonify
+from flask import Flask, abort, make_response, request, jsonify
 app = Flask(__name__)
 AUTH = Auth()
 
@@ -31,6 +31,29 @@ def req_user():
         return jsonify({"message": "email already registered"}), 400
 
     return jsonify({"email": user.email, "message": "user created"}), 200
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """ Login using form data. """
+    if not request.form:
+        return jsonify({"error": "missing form data"}), 400
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not email:
+        return jsonify({"error": "missing email"}), 400
+    if not password:
+        return jsonify({"error": "missing password"}), 400
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+    else:
+        abort(401)
+
+    res = make_response({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", session_id)
+    return res
 
 
 if __name__ == "__main__":
