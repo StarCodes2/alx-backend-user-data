c71773bbf0f9be5981eb48440b73d3970c92e4f6
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-12 08:21:25 +0000

    FEAT: Register a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Model to authenticate and register a user. """
 import bcrypt
+from db import DB, User, NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -10,3 +11,23 @@ def _hash_password(password: str) -> bytes:
     text = password.encode()
 
     return bcrypt.hashpw(text, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Register a new user if email doesn't exist in the database. """
+        if not email or not password:
+            return None
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+
+        return user
