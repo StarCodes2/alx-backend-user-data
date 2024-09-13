5d22790e6062001229833d1c9c1e9ac7c6fb425a
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-09-13 01:00:24 +0000

    FEAT: Flask route to get user password reset token.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -82,5 +82,18 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route("/reset_password", method=["POST"], strict_slashes=False)
+def get_reset_password_token():
+    """ Get a password reset token. """
+    email = request.form.get("email", None)
+    if not email:
+        return jsonify({"error": "Forbidden"}), 403
+    try:
+        token = self._db.get_reset_password_token(email)
+    except ValueError:
+        return jsonify({"error": "Forbidden"}), 403
+    return jsonify({"email": email, "reset_token": token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
